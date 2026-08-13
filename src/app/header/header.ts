ca274b59e387880d1700764f8865205c5bbc70ff
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-08-13 06:15:23 -0300

    first commit

---
+++ src/app/header/header.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  isMenuOpen:boolean = false;
+  
+  menuItems = [
+    { label: 'Home', link: '/' },
+    { label: 'Consultas', link: '/consultas' },
+    { label: 'Pacientes', link: '/pacientes' },
+    { label: 'Médicos', link: '/medicos' },
+    { label: 'Login', link: '/login' }
+  ];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
