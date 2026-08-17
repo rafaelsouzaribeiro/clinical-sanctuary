83d6672c0a57c2628ef5be7e1d6dac72b811c7f8
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-08-17 18:44:50 -0300

    add init perfil

--- src/app/perfil/perfil.ts
+++ src/app/perfil/perfil.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import doctorsData from './doctors.json';
+import commentsData from './comments.json';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
-export class Perfil {}
+export class Perfil {
+  public doctors = doctorsData;
+  public comments = commentsData;
+
+  ngOnInit():void{
+
+  }
+
+  get consultaLink():string[]{
+    return ['/consultas', this.doctors.slug, this.doctors.id];
+  }
+}
