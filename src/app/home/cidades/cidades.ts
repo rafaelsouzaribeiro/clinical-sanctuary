2b8c39a958e936701e97c48698c676ae3eb83d19
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-09-13 18:36:48 -0300

    search city name

--- src/app/home/cidades/cidades.ts
+++ src/app/home/cidades/cidades.ts
@@ -1,60 +1,95 @@
-import { Component, OnInit, Output, EventEmitter, output } from '@angular/core';
-import { CityOutput,StateOutput } from "./cidades.interface";
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CityOutput, StateOutput } from "./cidades.interface";
 import { HomeService } from '../../services/impl/home.service';
-import { UUID } from 'node:crypto';
 
 @Component({
   selector: 'app-cidades',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cidades.html',
   styleUrl: './cidades.css',
 })
 export class Cidades implements OnInit {
-    @Output() cityChange = new EventEmitter<string>();
-    @Output() searchChange = new EventEmitter<string>();
+  @Output() cityChange = new EventEmitter<string>();
+  @Output() searchChange = new EventEmitter<string>();
 
-    public ufOptions: StateOutput[] = [];
-    public cityOptions: CityOutput[] = [];
+  public ufOptions: StateOutput[] = [];
+  public cityOptions: CityOutput[] = [];
+  public filteredCityOptions: CityOutput[] = [];
 
-    public selectedID = '';
-    public selectedCity = '';
-    public searchTerm = '';
+  public selectedID = '';
+  public selectedCity = '';
+  public searchTerm = '';
 
-    constructor(private homeService: HomeService) {}
+  public cityInputValue = '';
+  public isCityDropdownOpen = false;
 
-    ngOnInit(): void {
-        this.homeService.getUfOptions().subscribe((options) => {
-            this.ufOptions = options;
-            this.selectedID = this.ufOptions[0]?.state_id ?? '';
-            this.loadCities(this.selectedID);
-        });
-    }
+  constructor(private homeService: HomeService) {}
 
-    private loadCities(state_id: string): void {
-        this.cityOptions = [];
-        this.selectedCity = '';
+  ngOnInit(): void {
+    this.homeService.getUfOptions().subscribe((options) => {
+      this.ufOptions = options;
+      this.selectedID = this.ufOptions[0]?.state_id ?? '';
+      this.loadCities(this.selectedID);
+    });
+  }
 
-        this.homeService.getCityOptions(state_id).subscribe((options) => {
-            this.cityOptions = options;
-            this.selectedCity = options[0]?.city_id ?? '';
-            this.cityChange.emit(this.selectedCity);
-        });
-    }
+  private loadCities(state_id: string): void {
+    this.cityOptions = [];
+    this.filteredCityOptions = [];
+    this.selectedCity = '';
+    this.cityInputValue = '';
 
-    onUfSelect(event: Event): void {
-        const value = (event.target as HTMLSelectElement).value;
-        this.selectedID = value;
-        this.loadCities(value);
-    }
+    this.homeService.getCityOptions(state_id).subscribe((options) => {
+      this.cityOptions = options;
+      this.filteredCityOptions = options;
 
-    onCitySelect(event: Event): void {
-        const value = (event.target as HTMLSelectElement).value;
-        this.selectedCity = value;
-        this.cityChange.emit(value);
-    }
+      const first = options[0];
+      if (first) {
+        this.selectedCity = first.city_id;
+        this.cityInputValue = first.name_city;
+        this.cityChange.emit(this.selectedCity);
+      }
+    });
+  }
 
-    onSearchChange(event: Event): void {
-        const value = (event.target as HTMLInputElement).value;
-        this.searchChange.emit(value);
-    }
+  onUfSelect(event: Event): void {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectedID = value;
+    this.loadCities(value);
+  }
+
+  onCityInputChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.cityInputValue = value;
+    this.isCityDropdownOpen = true;
+
+    const term = value.trim().toLowerCase();
+    this.filteredCityOptions = term
+      ? this.cityOptions.filter(c => c.name_city.toLowerCase().includes(term))
+      : this.cityOptions;
+  }
+
+  onCityInputFocus(): void {
+    this.isCityDropdownOpen = true;
+    this.filteredCityOptions = this.cityOptions;
+  }
+
+  onCityInputBlur(): void {
+    setTimeout(() => (this.isCityDropdownOpen = false), 150);
+  }
+
+  selectCity(option: CityOutput): void {
+    this.selectedCity = option.city_id;
+    this.cityInputValue = option.name_city;
+    this.isCityDropdownOpen = false;
+    this.cityChange.emit(this.selectedCity);
+  }
+
+  onSearchChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.searchTerm = value;
+    this.searchChange.emit(value);
+  }
 }
